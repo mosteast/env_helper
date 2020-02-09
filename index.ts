134437f9d63034418c97c5d3e81637bea9a483bf
kev <kev@localhost>
2020-02-10 06:21:52 +0800

    Auto commit: 2020-02-10_06:21:52

--- index.ts
+++ index.ts
@@ -2,40 +2,89 @@ import { pwd } from 'shelljs'
 import { resolve } from 'path'
 import { readFile, stat, writeFile } from 'fs-extra'
 import { DotenvParseOutput, parse } from 'dotenv'
+import { Invalid_argument } from './error/invalid_argument'
+
+enum A {
+  set     = 'set',
+  unset   = 'unset',
+  merge   = 'merge',
+  replace = 'replace',
+}
+
+export const N_edit_env_action = A
 
 interface T_opt_edit_env {
-  action: 'set' | 'unset'
-  key: string
-  value?: string | number
+  action: A
   /**
    * .env file path
    */
   path?: string
+  /**
+   * Whether reload process.env by .env file
+   */
+  reload?: boolean
+}
+
+interface T_opt_edit_env_set extends T_opt_edit_env {
+  action: A.set
+  key: string
+  value: string
+}
+
+interface T_opt_edit_env_unset extends T_opt_edit_env {
+  action: A.unset
+  key: string
+}
+
+interface T_opt_edit_env_merge extends T_opt_edit_env {
+  action: A.merge
+  map: DotenvParseOutput,
+}
+
+interface T_opt_edit_env_replace extends T_opt_edit_env {
+  action: A.replace
+  map: DotenvParseOutput,
 }
 
 /**
  * Set env variable by editing env file
  */
-export async function env_file_edit(opt: T_opt_edit_env) {
-  let { action, key, value, path } = { ...opt }
+export async function env_file_edit(opt: T_opt_edit_env_set | T_opt_edit_env_unset | T_opt_edit_env_merge | T_opt_edit_env_replace) {
+  let { action, key, value, map, path, reload } = { reload: false, ...opt }
+
   path = path || resolve(pwd().toString(), '.env')
-  const map: DotenvParseOutput = parse(await readFile(path)) || {}
-  console.log(map)
+  let r: DotenvParseOutput = parse(await readFile(path)) || {}
 
   switch (action) {
-    case 'set':
-      map[key] = '' + value
+    case A.set:
+      r[key] = '' + value
       break
-    case 'unset':
-      delete map[key]
+    case A.unset:
+      delete r[key]
+      break
+    case A.merge:
+      if (!map) {throw new Invalid_argument({ merge: map })}
+      r = { ...r, ...map }
+      break
+    case A.replace:
+      if (!map) {throw new Invalid_argument({ merge: map })}
+      r = map
       break
     default:
       throw new Error(`Invalid action ${action}`)
   }
 
-  await writeFile(path, env_encode(map))
+  await writeFile(path, env_encode(r))
+
+  if (reload) {
+    reload_env(path)
+  }
 }
 
+/**
+ * {a: 1, b: 2} ==> 'a=1\nb=2'
+ * @param map
+ */
 export function env_encode(map: DotenvParseOutput): string {
   let r = ''
 
@@ -46,15 +95,63 @@ export function env_encode(map: DotenvParseOutput): string {
   return r.trim()
 }
 
-export async function env_set(key: string, value: number | string, opt?: Partial<T_opt_edit_env>) {
-  await env_file_edit({ action: 'set', key, value, ...opt })
+/**
+ * Set an item in .env file
+ * @param key
+ * @param value
+ * @param opt
+ */
+export async function env_set(key: string, value: string, opt?: Partial<T_opt_edit_env_set>) {
+  await env_file_edit({
+    action: A.set,
+    key,
+    value, ...opt,
+  })
 }
 
-export async function env_unset(key: string, opt?: Partial<T_opt_edit_env>) {
-  await env_file_edit({ action: 'unset', key, ...opt })
+/**
+ * Unset an item in .env file
+ * @param key
+ * @param opt
+ */
+export async function env_unset(key: string, opt?: Partial<T_opt_edit_env_unset>) {
+  await env_file_edit({
+    action: A.unset,
+    key, ...opt,
+  })
 }
 
-export function reload_env_file(path: string) {
+/**
+ * Merge a object to .env file
+ * @param merge
+ * @param opt
+ */
+export async function env_merge(map: DotenvParseOutput, opt?: Partial<T_opt_edit_env_merge>) {
+  await env_file_edit({
+    action: A.merge,
+    map,
+    ...opt,
+  })
+}
+
+/**
+ * Replace all pairs in .env file
+ * @param map
+ * @param opt
+ */
+export async function env_replace(map: DotenvParseOutput, opt?: Partial<T_opt_edit_env_replace>) {
+  await env_file_edit({
+    action: A.replace,
+    map,
+    ...opt,
+  })
+}
+
+/**
+ * Reload env from a .env file
+ * @param path
+ */
+export function reload_env(path: string) {
   stat(path, e => {
     if (e) {
       console.warn(`.env file not found: ${path}`)
@@ -64,8 +161,12 @@ export function reload_env_file(path: string) {
   process.env.____ENV_LOADED____ = '1'
 }
 
+/**
+ * Reload if not loaded
+ * @param path
+ */
 export function load_env_once(path: string) {
   if (process.env.____ENV_LOADED____ == '1') {return}
 
-  reload_env_file(path)
+  reload_env(path)
 }
--- test/test.ts
+++ test/test.ts
@@ -1,30 +1,56 @@
-import { env_set, env_unset, reload_env_file } from '../index'
+import {
+  env_merge,
+  env_replace,
+  env_set,
+  env_unset,
+  reload_env,
+} from '../index'
 import { resolve } from 'path'
 import { readFileSync } from 'fs'
 import { truncate } from 'fs-extra'
 
 const path = resolve(__dirname, '.env.empty.test')
-beforeEach(async () => {
+
+async function clear() {
   await truncate(path)
-})
+}
+
+beforeEach(async () => {await clear()})
+afterEach(async () => {await clear()})
 
 it('env_set', async () => {
-  await env_set('a', 1, { path })
+  await env_set('a', '1', { path })
   expect(readFileSync(path).toString().trim()).toBe('a=1')
-  await env_set('b', 2, { path })
+  await env_set('b', '2', { path })
   expect(readFileSync(path).toString().trim()).toBe('a=1\nb=2')
 })
 
 it('env_unset', async () => {
-  await env_set('a', 1, { path })
-  await env_set('b', 2, { path })
+  await env_set('a', '1', { path })
+  await env_set('b', '2', { path })
   expect(readFileSync(path).toString().trim()).toBe('a=1\nb=2')
   await env_unset('b', { path })
   expect(readFileSync(path).toString().trim()).toBe('a=1')
 })
 
+it('env_merge', async () => {
+  await env_set('a', '1', { path })
+  await env_set('b', '2', { path })
+  expect(readFileSync(path).toString().trim()).toBe('a=1\nb=2')
+  await env_merge({ b: '3', c: '4' }, { path })
+  expect(readFileSync(path).toString().trim()).toBe('a=1\nb=3\nc=4')
+})
+
+it('env_replace', async () => {
+  await env_set('a', '1', { path })
+  await env_set('b', '2', { path })
+  expect(readFileSync(path).toString().trim()).toBe('a=1\nb=2')
+  await env_replace({ c: '3', d: '4' }, { path })
+  expect(readFileSync(path).toString().trim()).toBe('c=3\nd=4')
+})
+
 it('reload_env', async () => {
-  reload_env_file(resolve(__dirname, '.env.reload_env.test'))
+  reload_env(resolve(__dirname, '.env.reload_env.test'))
   expect(process.env.aa).toBe('1')
   expect(process.env.bb).toBe('2')
 })
